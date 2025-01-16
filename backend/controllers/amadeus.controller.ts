66482f0df5b7597602b3eec55ff19ce688bf1b60
mever-l <mor.lempert@gmail.com>
2025-01-16 20:43:55 +0200

    feat(flight-offers): begin impleamenting

--- backend/controllers/amadeus.controller.ts
+++ backend/controllers/amadeus.controller.ts
@@ -4,7 +4,7 @@ import * as querystring from "querystring";
 
 export const retreiveAmadeusToken = (request: Request, res: Response) => {
     const body = querystring.stringify({
-         grant_type: 'client_credentials',
+        grant_type: 'client_credentials',
         client_id: process.env.AMADEUS_CLIENT_ID,
         client_secret: process.env.AMADEUS_CLIENT_SECRET
     });
@@ -42,21 +42,40 @@ export const retreiveAmadeusToken = (request: Request, res: Response) => {
     req.end()
 } 
 
-export const flightOffersSearch = async (req: Request, res: Response) => {
-//   const body = querystring.stringify({});
-//   const options = {
-//   hostname: process.env.AMADEUS_API_BASE_URL,
-//   path: '/v2/shopping/flight-offers',
-//   agent: new https.Agent({ rejectUnauthorized: false }),
-// };
+export const flightOffersSearch = async (request: Request, res: Response) => {
+//   const body = querystring.stringify({
+//     currencyCode: 'ILS',
+//     originLocationCode: 'TLV',
+//     destinationLocationCode: 'ATH',
+//     departureDate: '2025-06-01',
+//     adults:1
+//   });
+  const options = {
+  hostname: process.env.AMADEUS_API_BASE_URL,
+  headers: { authorization:
+    `Bearer ${process.env.AMADEUS_ACCESS_TOKEN}`},
+  path: '/v2/shopping/flight-offers',
+  agent: new https.Agent({ rejectUnauthorized: false }),
+  method: 'GET'};
+    const req = https.request(options, ( response) => {
+        
+    let data = '';
+    response.on('data', (chunk) => {
+    data = data + chunk.toString();
+    });
 
-// try {
-//     await https.get(options, (response) => {
-//     res.send(response)
-//     res.status(200);
-//     });
-// } catch (error) {
-//     res.status(500)
-//     res.send(error)
+        response.on('end', () => {
+        const body = JSON.parse(data);
+        console.log(body);
+        res.status(response.statusCode).send(body);
+        });
+})
+
+    req.on('error', (error) => {
+    console.log('An error', error);
+    res.status(500).send(error.message);
+
+    });
+
+    req.end()
 }
-// }
