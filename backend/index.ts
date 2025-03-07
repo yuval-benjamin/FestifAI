99d09a6e0f1f40a6f566ee877f95fe48de34a09f
Yehuda Segal <ysegal@TEN-D7G229NMQV.local>
2025-03-07 14:29:14 +0200

    Open Ai Request works

--- backend/index.ts
+++ backend/index.ts
@@ -1,11 +1,10 @@
-import express, { Request, Response } from 'express';
-import { askChatGPT } from './chat.service';
+import express from 'express';
+import { festivalRouter } from './routers/festival.router';
 const app = express();
 const port = 3000;
 
-app.get('/test', (req: Request, res: Response) => {
-  askChatGPT(req, res);
-});
+app.use(express.json())
+app.use('/festivals',festivalRouter);
 
 app.listen(port, () => {
   console.log(`FestifAI server listening at http://localhost:${port}`);
