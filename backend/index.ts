bc8bb5d3614e83247c6d6df6069ff16bf8f79b54
Yehuda Segal <ysegal@TEN-D7G229NMQV.local>
2025-03-24 15:23:12 +0200

    rebase

--- backend/index.ts
+++ backend/index.ts
@@ -1,14 +1,15 @@
 import express, { Request, Response } from 'express';
 import { spotifyRouter } from './routers/spotify.route';
+import { askChatGPT } from './chat.service';
 import dotenv from "dotenv";
 
-dotenv.config(); 
+dotenv.config();
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello FestifAI!');
+app.get('/test', (req: Request, res: Response) => {
+  askChatGPT()
 });
 
 app.use('/spotify', spotifyRouter);
@@ -16,4 +17,3 @@ app.use('/spotify', spotifyRouter);
 app.listen(port, () => {
   console.log(`FestifAI server listening at http://localhost:${port}`);
 });
- 
