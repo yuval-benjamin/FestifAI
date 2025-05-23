249795fcab012cb9881761bfb298df4e0c5bc0d2
mever-l <mor.lempert@gmail.com>
2025-05-23 14:46:30 +0300

    feat(ui): yet again

--- client/src/components/Packages.tsx
+++ client/src/components/Packages.tsx
@@ -19,21 +19,23 @@ export const Packages: FC = () => {
        <div className=' d-flex flex-column justify-content-center align-items-center bangers-regular' 
     style={{
       width: "100%",
-      height: "100%", backgroundImage: "url(/fest.jpg)", backgroundSize: "cover", backgroundPosition: "center" 
+      height: "100%", backgroundImage: "url(/fest.jpg)", backgroundSize: "cover", 
+      backgroundPosition: "center",
     }}
     >
       <h1 className="display-1 bangers-regular text-center" style={{ color: "white" }}>choose your package</h1>
-      <div className="d-flex flex-row justify-content-center align-items-center">
+      <div className="d-flex flex-row justify-content-center align-items-center" style={{       marginBottom: '100px',
+}}>
         {packages?.map((festivalPackage) => (
-          <div key={festivalPackage._id} className="card text-center m-2" style={{
+          <div key={festivalPackage._id} className="card text-left m-2" style={{
             width: "20rem",
             height: "25rem",
             backgroundColor: "rgba(31, 31, 61, 0.8)",
             color: "white",
             border: "none",
           }} onClick={() => onSelectPackage(festivalPackage)}>
-            <div className="card-body">
-              <h5 className="card-title bangers-regular">{festivalPackage.packageType}</h5>
+            <div className="card-body" style={{fontSize: '18px'}}>
+              <h5 className="card-title bangers-regular text-center"style={{fontSize:'22px'}}>{festivalPackage.packageType}</h5>
               <p className="card-text">dates: {(festivalPackage.startDay)} til {festivalPackage.endDay}</p>
                 { (festivalPackage.flights.departure[0].airline == festivalPackage.flights.return[0].airline) ? 
               <p className="card-text">airline: {festivalPackage.flights.departure[0].airline}</p>
