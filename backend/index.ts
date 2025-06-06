7b32bf51854cbf3ca49c44771d1daca7c2bc54cb
yuval-benjamin <cs100@ST25-100.cs.colman.ac.il>
2025-06-07 01:16:18 +0300

    backend is up

--- backend/index.ts
+++ backend/index.ts
@@ -6,12 +6,16 @@ import dotenv from "dotenv";
 import { amadeusRouter } from './routers/amadeus.router';
 import cors from 'cors';
 import { categoryRouter } from './routers/category.router';
+import https from 'https';
+import http from 'http';
+import fs from 'fs';
+
 dotenv.config();
 connectToMongoDB();
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 const app = express();
-const port = 3000;
+const port: string = process.env.PORT || "3000";
 
 app.use(cors());
 
@@ -24,6 +28,18 @@ app.use('/spotify', spotifyRouter);
 app.use('/amadeus', amadeusRouter);
 
 
-app.listen(port, () => {
-  console.log(`FestifAI server listening at http://localhost:${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`FestifAI server listening at http://localhost:${port}`);
+// });
+
+if(process.env.NODE_ENV !== 'production') {
+  console.log("development env");
+  http.createServer(app).listen(port)
+} else {
+  const options = {
+    key: fs.readFileSync('./client-key.pem'),
+    cert: fs.readFileSync('./client-cert.pem')
+  }
+  console.log("production env")
+  https.createServer(options, app).listen(443)
+}
